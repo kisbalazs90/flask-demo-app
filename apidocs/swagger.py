0666fbfb6b1da0f021c561defab752167916f924
Kis Balázs <kis.balazs@e-vista.hu>
2025-02-02 12:00:46 +0100

    Add security (JWT) extend Swagger config

--- apidocs/swagger.py
+++ apidocs/swagger.py
@@ -1,5 +1,78 @@
 # swagger_annotations.py
 
+swagger_config = {
+    "title": "Book API",
+    "description": "API documentation for Book Management",
+    "version": "1.0",
+    "uiversion": 3,
+    "authorizations": {
+        "Bearer": {
+            "type": "apiKey",
+            "in": "header",
+            "name": "Authorization",
+            "description": "JWT Bearer token authentication"
+        }
+    },
+    "securityDefinitions": {
+        "Bearer": {
+            "type": "apiKey",
+            "in": "header",
+            "name": "Authorization",
+            "description": "JWT Bearer token"
+        }
+    },
+    "security": [{"Bearer": []}],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/{}.json'.format('apispec_1'),
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "headers": [],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
+
+
+LOGIN_ANNOTATION =      """
+    User Login
+    ---
+    tags:
+      - Authentication
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            username:
+              type: string
+              example: "admin"
+            password:
+              type: string
+              example: "admin"
+    responses:
+      200:
+        description: Successfully authenticated
+        schema:
+          type: object
+          properties:
+            access_token:
+              type: string
+              example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+      401:
+        description: Invalid credentials
+        schema:
+          type: object
+          properties:
+            msg:
+              type: string
+              example: "Bad username or password"
+    """
 
 GET_BOOKS_ANNOTATION = """
 Get a list of all books
@@ -7,6 +80,8 @@ Get a list of all books
 responses:
   200:
     description: A list of books
+security:
+  - Bearer: []
 """
 
 POST_BOOK_ANNOTATION = """
@@ -28,6 +103,8 @@ parameters:
 responses:
   201:
     description: Book created successfully
+security:
+  - Bearer: []
 """
 
 GET_BOOKS_BY_ID_ANNOTATION = """
@@ -43,6 +120,8 @@ responses:
     description: Book found
   404:
     description: Book not found
+security:
+  - Bearer: []
 """
 
 UPDATE_BOOK_ANNOTATION = """
@@ -70,6 +149,8 @@ responses:
     description: Book updated successfully
   404:
     description: Book not found
+security:
+  - Bearer: []
 """
 
 DELETE_BOOK_ANNOTATION = """Delete a book by ID
@@ -84,4 +165,6 @@ responses:
     description: Book deleted successfully
   404:
     description: Book not found
+security:
+  - Bearer: []
 """
--- app.py
+++ app.py
@@ -1,49 +1,79 @@
 # app.py
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
-from flask import Flask, request, jsonify
-from flasgger import Swagger
+
+from apidocs.swagger import *
 from dotenv import load_dotenv
-import os
+from flasgger import Swagger
+from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from resources.book.book_model import Book
 from resources.book.book_service import book_find_all, book_find_one, book_create, book_update, book_delete
-from apidocs.swagger import *
+import os
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-swagger = Swagger(app)
+app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET')
 
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+
+jwt = JWTManager(app)
+swagger = Swagger(app, config=swagger_config)
+
+@app.route("/login", methods=["POST"])
+def login():
+    built_in_user = os.getenv('USER_NAME')
+    built_in_password = os.getenv('USER_PASSWORD')
+    print(built_in_user, built_in_password)
+
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    if username != built_in_user or password != built_in_password:
+        return {"msg": "Bad username or password"}, 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+login.__doc__ = LOGIN_ANNOTATION
+
+
 @app.route('/books', methods=['GET'])
+@jwt_required()
 def get_books():
   return book_find_all()
 get_books.__doc__ = GET_BOOKS_ANNOTATION
-
-@app.route('/books', methods=['POST'])
-def add_book():
-  data = request.get_json()
-  return book_create(data)
-add_book.__doc__ = POST_BOOK_ANNOTATION
 
 @app.route('/books/<int:book_id>', methods=['GET'])
 def get_book(book_id):
   book_find_one(book_id)
 get_book.__doc__ = GET_BOOKS_BY_ID_ANNOTATION
 
+@app.route('/books', methods=['POST'])
+@jwt_required()
+def add_book():
+  data = request.get_json()
+  return book_create(data)
+add_book.__doc__ = POST_BOOK_ANNOTATION
+
 @app.route('/books/<int:book_id>', methods=['PUT'])
+@jwt_required()
 def update_book(book_id):
   data = request.get_json()
   return book_update(book_id, data)
 update_book.__doc__ = UPDATE_BOOK_ANNOTATION
 
 @app.route('/books/<int:book_id>', methods=['DELETE'])
+@jwt_required()
 def delete_book(book_id):
     return book_delete(book_id);
 delete_book.__doc__ = DELETE_BOOK_ANNOTATION
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
